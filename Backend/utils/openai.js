c24e2ad9bbe9f7a95d91d1b6396575623c3fda24
lokeshlikhar <lokeshlikhar01@gmail.com>
2026-08-19 22:29:44 +0530

    update in groq model

--- Backend/utils/openai.js
+++ Backend/utils/openai.js
@@ -15,7 +15,7 @@ const getOpenAiResponse = async (message) => {
       Authorization: `Bearer ${process.env.GROQ_API_KEY}`,
     },
     body: JSON.stringify({
-      model: "llama-3.3-70b-versatile",
+      model: "meta-llama/llama-prompt-guard-2-86m",
       messages: [
         {
           role: "user",
